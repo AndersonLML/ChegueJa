12977da58df00ee7e9f5fd8aeb24633388a5ad1b
Anderson Leite <gtlt1999@gmail.com>
2023-03-16 20:27:35 -0300

    update store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
-import ReactoTron from '../Config/reactotron';
+import ReactoTron from '../config/reactotron';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store  = createStore(
     rootReducer,
-    compose(applyMiddleware(sagaMiddleware), ReactoTron.creareEnhancer()),    
+    compose(applyMiddleware(sagaMiddleware), ReactoTron.createEnhancer()),    
 );
 
 sagaMiddleware.run(rootSaga);
