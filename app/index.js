48ff791d07f3081d981b94218d06ffc4a2c66e77
Anderson Leite <gtlt1999@gmail.com>
2023-03-16 21:31:52 -0300

    adicionando as rotas

--- app/index.js
+++ app/index.js
@@ -2,19 +2,15 @@ import { React } from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
+import Routes from './src/routes'
 import { Provider } from 'react-redux';
 import store from './src/store';
 
-import Login from './src/pages/Login';
-import Type from './src/pages/steps/type';
-import Car from './src/pages/steps/car'
-import Payment from './src/pages/steps/payment';
-import Ride from './src/pages/Ride';
-import Home from './src/pages/Home';
+
 
 const App = () => (
     <Provider store={store}>
-    <Home />
+    <Routes />
 </Provider>
 );
 
---
+++ app/src/routes/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+    import {navigationRef} from './rootNavigation';
+
+import Login from '../pages/Login';
+import Type from '../pages/steps/type';
+import Car from '../pages/steps/car'
+import Payment from '../pages/steps/payment';
+import Ride from '../pages/Ride';
+import Home from '../pages/Home';
+
+const Stack = createStackNavigator();
+
+const Routes = () => (
+    <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+                options={{headerShown: false}}
+                name="Login"
+                component={Login}
+            />
+             <Stack.Screen
+                options={{headerShown: false}}
+                name="Type"
+                component={Type}
+            />
+             <Stack.Screen
+                options={{headerShown: false}}
+                name="Car"
+                component={Car}
+            />
+             <Stack.Screen
+                options={{headerShown: false}}
+                name="Payment"
+                component={Payment}
+            />
+             <Stack.Screen
+                options={{headerShown: false}}
+                name="Ride"
+                component={Ride}
+            />
+             <Stack.Screen
+                options={{headerShown: false}}
+                name="Home"
+                component={Home}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
+
+export default Routes;
